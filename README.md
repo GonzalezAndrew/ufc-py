babd80bfa84b6fa1048b4e9e663f7b17aa852959
Andrew Gonzalez <andrewgonzo23@gmail.com>
2021-11-01 23:31:24 -0500

    adding pre-commit

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # UFC Stats Python Module
 
 A Python module used to retrieve data from the [UFC Stats website](http://www.ufcstats.com).
-
-
--- ufc/utils.py
+++ ufc/utils.py
@@ -1,5 +1,6 @@
 import requests
-from constants import completed_events_url, upcoming_events_url, base_url
+from constants import base_url
+
 
 def get_data(endpoint: str) -> str:
     """
@@ -8,14 +9,14 @@ def get_data(endpoint: str) -> str:
     :return: The data from the endpoint.
     :rtype: str | None
     """
-    data = requests.get(url=f"{base_url}{endpoint}")
+    data = requests.get(url=f'{base_url}{endpoint}')
     try:
         data.raise_for_status()
     except requests.exceptions.HTTPError as err:
         print(err)
-        return ""
-    
-    if data.text != "":
+        return ''
+
+    if data.text != '':
         return data.text
     else:
-        return ""
+        return ''
